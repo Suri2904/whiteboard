0cbabd8c0fbddacf5d31b3b61956a6f71d8d6840
Suryansh S Chandel <59773944+Suri2904@users.noreply.github.com>
2026-05-20 14:24:02 +0530

    Add files via upload
    Must have Should have added

--- sw.js
+++ sw.js
@@ -1,20 +1,29 @@
-const CACHE = 'whiteboard-v1';
-const FILES = ['/', '/index.html', '/manifest.json'];
+const CACHE='wb-v5';
+const STATIC=[
+  '/','/index.html','/manifest.json','/icon-192.png','/icon-512.png'
+];
 
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
+self.addEventListener('install',e=>{
+  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(STATIC)));
   self.skipWaiting();
 });
-
-self.addEventListener('activate', e => {
-  e.waitUntil(caches.keys().then(keys =>
-    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+self.addEventListener('activate',e=>{
+  e.waitUntil(caches.keys().then(keys=>
+    Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))
   ));
   self.clients.claim();
 });
-
-self.addEventListener('fetch', e => {
+self.addEventListener('fetch',e=>{
   e.respondWith(
-    caches.match(e.request).then(r => r || fetch(e.request))
+    caches.match(e.request).then(r=>{
+      if(r)return r;
+      return fetch(e.request).then(res=>{
+        if(res&&res.status===200&&e.request.method==='GET'){
+          const clone=res.clone();
+          caches.open(CACHE).then(c=>c.put(e.request,clone));
+        }
+        return res;
+      }).catch(()=>caches.match('/index.html'));
+    })
   );
 });
